4bb554da262d2136ea9ca619cd7cce3abbd55d54
Jack Veney <46730283+jackveney@users.noreply.github.com>
2023-03-03 17:41:22 -0500

    Update chroot_tasks.sh

--- chroot_tasks.sh
+++ chroot_tasks.sh
@@ -4,31 +4,9 @@
 # install packages we need (build-essential is temporary)
 apt-get -y install xserver-xorg xserver-xorg-video-all \
 	chromium unclutter ifplugd xinit blackbox \
-	rbenv ruby-dev build-essential \
-	nano screen git-core ntpdate openssh-server sddm
+	network-manager \
+	nano screen ntpdate openssh-server sddm
 	
-rbenv install 1.9.1-p431
-rbenv global 1.9.1-p431
-
-# and rubygems we need
-#gem install bandshell
-cat > /tmp/install_bandshell.sh <<EOF
-#!/bin/sh -e
-cd /tmp
-git clone https://github.com/concerto/bandshell.git
-cd bandshell
-gem build bandshell.gemspec
-gem install *.gem
-cd /
-rm -rf /tmp/bandshell
-EOF
-chmod +x /tmp/install_bandshell.sh
-/tmp/install_bandshell.sh
-
-# once rubygems have been installed, build-essential isn't needed
-apt-get -y purge build-essential
-apt-get -y autoremove
-
 # clean up apt caches
 apt-get -y clean
 
@@ -36,8 +14,27 @@ apt-get -y clean
 # will start the X server and the player
 useradd -m -s `which xinit` concerto
 
-# create a .xinitrc that will start fullscreen chromium
+# create a url.sh that defines the screen URL
+cat > /home/concerto/url.sh << "EOF"
+#!/bin/sh
+URL=""
 
+EOF
+
+# create a .xinitrc that will start fullscreen chromium
+cat > /home/concerto/.xsessionrc << "EOF"
+#!/bin/sh
+source url.sh
+
+# hide the mouse pointer
+unclutter &
+# disable power-management and screen blanking
+xset -dpms
+xset s off
+
+# run the browser (if it crashes or dies, the X session should end)
+/usr/bin/chromium --noerrdialogs --disable-translate --disable-infobars --no-first-run --kiosk $URL
+EOF
 
 mkdir -p /etc/sddm.conf.d
 
@@ -49,17 +46,3 @@ Relogin=false
 EOF
 
 systemctl enable sddm
-
-# create systemctl file to start bandshell
-cat > /etc/systemd/system/bandshell.service << EOF
-[Unit]
-Description= bandshell for concerto
-
-[Service]
-ExecStart=/bin/bash /usr/local/bin/bandshelld start
-
-[Install]
-WantedBy= multi-user.target
-EOF
-
-systemctl enable bandshell.service
