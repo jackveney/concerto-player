25db38fa26f1859c6e8ac6763cda4173fa1707a2
Jack Veney <46730283+jackveney@users.noreply.github.com>
2023-03-03 18:25:58 -0500

    Update chroot_tasks.sh

--- chroot_tasks.sh
+++ chroot_tasks.sh
@@ -20,23 +20,24 @@ apt-get install webmin -y
 # will start the X server and the player
 useradd -m -s `which xinit` concerto
 
-# create a url.sh that defines the screen URL
-cat > /home/concerto/url.sh << "EOF"
-#!/bin/sh
-URL=""
-
 EOF
 
 # create a .xinitrc that will start fullscreen chromium
 cat > /home/concerto/.xsessionrc << "EOF"
 #!/bin/sh
-source url.sh
 
-# hide the mouse pointer
-unclutter &
-# disable power-management and screen blanking
-xset -dpms
+#Set the URL for chrome to open
+URL="https://concerto.wpctech.info/frontend/2"
+
+# ----------- DO NOT EDIT BELOW THIS LINE ----------- #
+xset s noblank
 xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
 
 # run the browser (if it crashes or dies, the X session should end)
 /usr/bin/chromium --noerrdialogs --disable-translate --disable-infobars --no-first-run --kiosk $URL
